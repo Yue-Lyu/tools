443f07f36deb2e9b1494211e4ad0f595b49c18bd
Yue <yl4259@columbia.edu>
2019-11-30 22:04:02 +0000

    create super user and  add admin.py to the app.

--- sightings/views.py
+++ sightings/views.py
@@ -1,6 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Squirrels
 
-def index(request):
-    return HttpResponse('our app')
+def all_squirrels(request):
+    squirrels = Squirrels.objects.all()
+    context = {
+            'squirrels':squirrels,
+            }
+    return render(request,'sightings/all.html',context)
+def squirrels_details(request,Unique_squirrel_id):
+    squirrel = Squirrels.objects.get(id=Unique_squirrel_id)
+    return HttpResponse(f"hi,i'm {squirrel.Unique_squirrel_id}")
+
 # Create your views here.
